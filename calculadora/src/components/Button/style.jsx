87b6b9a098d04a567755c4426ef2e1710bb7e5bb
Fabiano Kopper Vieira <131024637+FabianoKopperVieira@users.noreply.github.com>
2024-11-06 09:28:48 -0300

    Initial commit

---
+++ calculadora/src/components/Button/style.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+
+
+export const ButtonContainer = styled.button`
+
+    padding: 20px;
+    boder: 1px solid #CDCDCD;
+    background-color: gray;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weigth: 700;
+    flex:1;
+
+    &:hover {
+        opacity:0.6;
+    }
+
+
+`
---
+++ calculadora/src/components/Input/style.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+
+
+ export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: black;
+    display:flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Roboto';
+   
+   
+    
+
+    input{
+        width: 100%;
+        heigth: 75px;
+        background-color: black;
+        padding: 0 10px;    
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+        border: none;      /* Remove a borda */
+        outline: none;     /* Remove o contorno */
+    
+    
+    
+        }
+
+`
+
